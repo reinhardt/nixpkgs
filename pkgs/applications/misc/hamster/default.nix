42c04fefc137fd86b48cbcc20fdffe895359769a
Manuel Reinhardt <manuel.reinhardt@neon-cathedral.net>
2015-04-05 23:11:34 +0100

    use generic builder

--- pkgs/applications/misc/hamster/default.nix
+++ pkgs/applications/misc/hamster/default.nix
@@ -2,10 +2,11 @@
 
 stdenv.mkDerivation {
   name = "hamster-1.04";
-  builder = ./builder.sh;
   src = fetchurl {
     url = https://github.com/projecthamster/hamster/archive/hamster-time-tracker-1.04.tar.gz;
     md5 = "148ffee0c26c5c4ddfa4ebc9a21fa860";
   };
-  inherit gconf gettext gtk3 intltool python pythonDBus;
+  buildPhase = "python ./waf configure build --prefix=$out";
+  installPhase = "python ./waf install";
+  buildInputs = [ gconf gettext gtk3 intltool python pythonDBus ];
 }
